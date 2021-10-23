e0706fac56e580f03859f798ccfdaa068aebb928
EmptyProgrammer <EmptyProgrammer@gmail.com>
2021-10-23 05:27:09 +0330

    home-nav kinda fixed .

--- src/components/HomePage/HomeBann.js
+++ src/components/HomePage/HomeBann.js
@@ -13,7 +13,7 @@ export default function HomeBann() {
           <CtaLogoOne src="/images/cta-logo-one.svg" />
           <CtaBtn>GET THE DISNEY BUNDLE</CtaBtn>
           {bannData.map((item) => {
-            return <Descripction>{item.txt}</Descripction>;
+            return <Descripction key={item.id}>{item.txt}</Descripction>;
           })}
           <CtaLogoTwo src="/images/cta-logo-two.png" />
         </Cta>
@@ -58,44 +58,42 @@ const Content = styled.div`
 const Cta = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 700px;
+  max-width: 650px;
   width: 100%;
 `;
 
 const CtaLogoOne = styled.img`
-margin-bottom: 10px;
-min-height: 1px;
-display: block;
-width: 100%;
+  margin-bottom: 10px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
 `;
 
 const CtaLogoTwo = styled.img`
-margin-bottom: 10px;
-display: block;
-width: 100%;
+  margin-bottom: 10px;
+  display: block;
+  width: 100%;
 `;
 
 const CtaBtn = styled.a`
-font-weight: bold;
-color: #f9f9f9;
-background-color: #0063e5;
-width: 100%;
-margin-bottom: 10px;
-padding: 17px 0;
-border: 1px solid transparent;
-border-radius: 4px;
-&:hover {
-  background-color: #0483ee;
-  transition: all 0.2s ;
-}
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 17px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  &:hover {
+    background-color: #0483ee;
+    transition: all 0.2s;
+  }
 `;
 
 const Descripction = styled.p`
-color: hsla(0, 0%, 95% , 1);
-font-size: 12px;
-margin: 0 0 15px;
-line-height: 1.5;
-letter-spacing: 1px;
+  color: hsla(0, 0%, 95%, 1);
+  font-size: 12px;
+  margin: 0 0 15px;
+  line-height: 1.5;
+  letter-spacing: 1px;
 `;
-
-
--- src/components/HomePage/HomeNav.js
+++ src/components/HomePage/HomeNav.js
@@ -4,19 +4,37 @@ import styled from 'styled-components'
 export default function HomeNav() {
     return (
         <Main>
-            <img src="" alt="" />
-            <button className="logIn">
-                Login
-            </button>
+            <NavLogo src="/images/logo.svg" />
+            <NavBtn>LOG IN</NavBtn>
         </Main>
     )
 }
 
-const Main = styled.div`
+const Main = styled.nav`
 display: flex;
+justify-content: space-between;
+height: 70px;
+align-items: center;
+padding: 0 30px;
+padding-top: 10px;
+`;
 
-.logIn {
+const NavLogo = styled.img`
+width: 100px;
+`;
 
-}
-
+const NavBtn = styled.button`
+height: 35px;
+width: 80px;
+font-size: 14px;
+color: #f9f9f9;
+border: 1px solid;
+border-radius: 4px;
+background: none;
+cursor: pointer;
+&:hover {
+    background-color: #f9f9f9;
+    color: black;
+    transition: all 0.2s;
+  }
 `;
