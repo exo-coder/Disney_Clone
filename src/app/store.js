2aa79187d8fbd48c2e02450aa02fe6320d1d17b1
EmptyProgrammer <EmptyProgrammer@gmail.com>
2021-10-26 22:52:04 +0330

    Movies compo- and movie  data added => we added it to the redux ... now we can use it

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../event/user/userSlice"
+import movieReducer from "../event/movie/movieSlice";
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    movie: movieReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
---
+++ src/components/SitePage/SiteMovies.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function SiteMovies() {
+    return (
+        <Main>
+            
+        </Main>
+    )
+}
+
+const Main = styled.div`
+
+`;
